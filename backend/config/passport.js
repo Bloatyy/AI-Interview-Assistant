4dbabf115b1451df31cdee308232ddf9bb0b3bfb
Bloatyy <kunalsrathore99@gmail.com>
2026-05-08 02:59:24 +0530

    Add authentication backend with Email/Magic Link and Google OAuth support

---
+++ backend/config/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        user = await User.findOne({ email: profile.emails[0].value });
+        if (user) {
+          // Update existing user with googleId
+          user.googleId = profile.id;
+          user.isVerified = true;
+          await user.save();
+        } else {
+          // Create new user
+          user = await User.create({
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            name: profile.displayName,
+            isVerified: true
+          });
+        }
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
